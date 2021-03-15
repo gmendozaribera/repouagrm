06a761d27fdaa3561001d4797e90f02c2652092c
Willan <willrod19.9591@gmail.com>
2021-03-15 19:24:33 -0400

    Añadiendo Git ignore

--- project_git/urls.py
+++ project_git/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', include('users.urls')),
     #path('', RedirectView.as_view(url='login/', permanent=True)),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
