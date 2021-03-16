5c912a7febc32873d1d1d31fdb5fbe1564c41a83
Willan <willrod19.9591@gmail.com>
2021-03-16 12:15:26 -0400

    sqlite3

--- project_git/settings.py
+++ project_git/settings.py
@@ -82,6 +82,12 @@ WSGI_APPLICATION = 'project_git.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+""" DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'proyecto_git',
         'USER': 'root', #
@@ -89,7 +95,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
-}
+} """
 
 """ DATABASES = {
     'default': {
